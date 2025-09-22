83c67afc5e814e9ebfd43210630ad588c86ab631
coderskm <sumitkumarmishra.code@gmail.com>
2025-09-22 15:07:54 +0530

    DB connection

--- src/app.js
+++ src/app.js
@@ -1,12 +1,35 @@
 const express = require('express')
-
+const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
 
-app.use("/test",(req, res) => {
-    res.send("Hello from the server");
-})
-app.use("/",(req, res) => {
-    res.send("Hello from the server home");
+app.post("/signup", async (req, res) => {
+try {
+    const userObj = {
+      firstName: "Akshay",
+      lastName: "Saini",
+      emailId: "akshay@saini.com",
+      password: "akshay@123",
+    };
+
+    // creating a new instance of user model
+    const user = new User(userObj);
+    // saving into DB, returns promise
+    await user.save();
+    res.status(201).send("user saved successfully");
+} catch (error) {
+  res.status(400).send("Error in saving user", error.message);
+}
 })
 
-app.listen(3000,()=>console.log("server running on PORT 3000"));
+connectDB()
+  .then(() => {
+    console.log("Database connection established ...");
+    app.listen(3000, () => console.log("server running on PORT 3000"));
+
+  })
+  .catch((err) => {
+    console.log("Database cannot be connected");
+  });
+
+
